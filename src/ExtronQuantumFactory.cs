56fd6810ab3352097a7abc3168df3d4899dbb96d
Andrew Welker <awelker@pepperdash.com>
2023-05-01 09:55:37 -0600

    feat: add device info parsing

--- src/ExtronQuantumFactory.cs
+++ src/ExtronQuantumFactory.cs
@@ -1,68 +1,68 @@
-﻿using System.Collections.Generic;
-using PepperDash.Core;
+﻿using PepperDash.Core;
 using PepperDash.Essentials.Core;
+using System.Collections.Generic;
 
-namespace EssentialsPluginTemplate
+namespace epi.switcher.extron.quantum
 {
-	/// <summary>
-	/// Plugin device factory for devices that use IBasicCommunication
-	/// </summary>
-	/// <remarks>
-	/// Rename the class to match the device plugin being developed
-	/// </remarks>
-	/// <example>
-	/// "EssentialsPluginFactoryTemplate" renamed to "MyDeviceFactory"
-	/// </example>
+    /// <summary>
+    /// Plugin device factory for devices that use IBasicCommunication
+    /// </summary>
+    /// <remarks>
+    /// Rename the class to match the device plugin being developed
+    /// </remarks>
+    /// <example>
+    /// "EssentialsPluginFactoryTemplate" renamed to "MyDeviceFactory"
+    /// </example>
     public class ExtronQuantumFactory : EssentialsPluginDeviceFactory<ExtronQuantumDevice>
     {
-		/// <summary>
-		/// Plugin device factory constructor
-		/// </summary>
-		/// <remarks>
-		/// Update the MinimumEssentialsFrameworkVersion & TypeNames as needed when creating a plugin
-		/// </remarks>
-		/// <example>
- 		/// Set the minimum Essentials Framework Version
-		/// <code>
-		/// MinimumEssentialsFrameworkVersion = "1.6.4;
-        /// </code>
-		/// In the constructor we initialize the list with the typenames that will build an instance of this device
+        /// <summary>
+        /// Plugin device factory constructor
+        /// </summary>
+        /// <remarks>
+        /// Update the MinimumEssentialsFrameworkVersion & TypeNames as needed when creating a plugin
+        /// </remarks>
+        /// <example>
+        /// Set the minimum Essentials Framework Version
         /// <code>
-		/// TypeNames = new List<string>() { "SamsungMdc", "SamsungMdcDisplay" };
+        /// MinimumEssentialsFrameworkVersion = "1.6.4;
         /// </code>
-		/// </example>
+        /// In the constructor we initialize the list with the typenames that will build an instance of this device
+        /// <code>
+        /// TypeNames = new List<string>() { "SamsungMdc", "SamsungMdcDisplay" };
+        /// </code>
+        /// </example>
         public ExtronQuantumFactory()
         {
             // Set the minimum Essentials Framework Version
-			// TODO [ ] Update the Essentials minimum framework version which this plugin has been tested against
-			MinimumEssentialsFrameworkVersion = "1.13.3";
+            // TODO [ ] Update the Essentials minimum framework version which this plugin has been tested against
+            MinimumEssentialsFrameworkVersion = "1.13.3";
 
             // In the constructor we initialize the list with the typenames that will build an instance of this device
-			// TODO [ ] Update the TypeNames for the plugin being developed
+            // TODO [ ] Update the TypeNames for the plugin being developed
             TypeNames = new List<string>() { "extronQuantum" };
         }
-        
-		/// <summary>
-		/// Builds and returns an instance of EssentialsPluginDeviceTemplate
-		/// </summary>
-		/// <param name="dc">device configuration</param>
-		/// <returns>plugin device or null</returns>
-		/// <remarks>		
-		/// The example provided below takes the device key, name, properties config and the comms device created.
-		/// Modify the EssetnialsPlugingDeviceTemplate constructor as needed to meet the requirements of the plugin device.
-		/// </remarks>
-		/// <seealso cref="PepperDash.Core.eControlMethod"/>
+
+        /// <summary>
+        /// Builds and returns an instance of EssentialsPluginDeviceTemplate
+        /// </summary>
+        /// <param name="dc">device configuration</param>
+        /// <returns>plugin device or null</returns>
+        /// <remarks>		
+        /// The example provided below takes the device key, name, properties config and the comms device created.
+        /// Modify the EssetnialsPlugingDeviceTemplate constructor as needed to meet the requirements of the plugin device.
+        /// </remarks>
+        /// <seealso cref="eControlMethod"/>
         public override EssentialsDevice BuildDevice(PepperDash.Essentials.Core.Config.DeviceConfig dc)
         {
             Debug.Console(1, $"[{dc.Key}] Factory Attempting to create new device from type: ${dc.Type}");
-            
+
             var propertiesConfig = dc.Properties.ToObject<ExtronQuantumConfig>();
             if (propertiesConfig == null)
             {
                 Debug.Console(0, $"[{dc.Key}] Factory: failed to read properties config for ${dc.Name}");
                 return null;
-            }            
-                                    
+            }
+
             var comms = CommFactory.CreateCommForDevice(dc);
             if (comms == null)
             {
@@ -78,4 +78,3 @@ namespace EssentialsPluginTemplate
     }
 }
 
-          
--- src/ExtronQuantumJoinMap.cs
+++ src/ExtronQuantumJoinMap.cs
@@ -1,56 +1,56 @@
 ﻿using PepperDash.Essentials.Core;
 
-namespace EssentialsPluginTemplate
+namespace epi.switcher.extron.quantum
 {
-	/// <summary>
-	/// Plugin device Bridge Join Map
-	/// </summary>
-	/// <remarks>
-	/// Rename the class to match the device plugin being developed.  Reference Essentials JoinMaps, if one exists for the device plugin being developed
-	/// </remarks>
-	/// <see cref="PepperDash.Essentials.Core.Bridges"/>
-	/// <example>
-	/// "EssentialsPluginBridgeJoinMapTemplate" renamed to "SamsungMdcBridgeJoinMap"
-	/// </example>
-	public class ExtronQuantumJoinMap : JoinMapBaseAdvanced
-	{
-		#region Digital
-		
-
-		[JoinName("IsOnline")]
-		public JoinDataComplete IsOnline = new JoinDataComplete(
-			new JoinData
-			{
-				JoinNumber = 1,
-				JoinSpan = 1
-			},
-			new JoinMetadata
-			{
-				Description = "Is Online",
-				JoinCapabilities = eJoinCapabilities.ToSIMPL,
-				JoinType = eJoinType.Digital
-			});		
-
-		#endregion
+    /// <summary>
+    /// Plugin device Bridge Join Map
+    /// </summary>
+    /// <remarks>
+    /// Rename the class to match the device plugin being developed.  Reference Essentials JoinMaps, if one exists for the device plugin being developed
+    /// </remarks>
+    /// <see cref="PepperDash.Essentials.Core.Bridges"/>
+    /// <example>
+    /// "EssentialsPluginBridgeJoinMapTemplate" renamed to "SamsungMdcBridgeJoinMap"
+    /// </example>
+    public class ExtronQuantumJoinMap : JoinMapBaseAdvanced
+    {
+        #region Digital
 
 
-		#region Analog
+        [JoinName("IsOnline")]
+        public JoinDataComplete IsOnline = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 1,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Is Online",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Digital
+            });
 
-		// TODO [ ] Add analog joins below plugin being developed
+        #endregion
 
-		[JoinName("Status")]
-		public JoinDataComplete Status = new JoinDataComplete(
-			new JoinData
-			{
-				JoinNumber = 1,
-				JoinSpan = 1
-			},
-			new JoinMetadata
-			{
-				Description = "Socket Status",
-				JoinCapabilities = eJoinCapabilities.ToSIMPL,
-				JoinType = eJoinType.Analog
-			});
+
+        #region Analog
+
+        // TODO [ ] Add analog joins below plugin being developed
+
+        [JoinName("Status")]
+        public JoinDataComplete Status = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 1,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Socket Status",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Analog
+            });
 
         [JoinName("PresetSelect")]
         public JoinDataComplete PresetSelect = new JoinDataComplete(
@@ -105,17 +105,17 @@ namespace EssentialsPluginTemplate
         // TODO [ ] Add serial joins below plugin being developed
         [JoinName("DeviceName")]
         public JoinDataComplete DeviceName = new JoinDataComplete(
-			new JoinData
-			{
-				JoinNumber = 1,
-				JoinSpan = 1
-			},
-			new JoinMetadata
-			{
-				Description = "Device Name",
-				JoinCapabilities = eJoinCapabilities.ToSIMPL,
-				JoinType = eJoinType.Serial
-			});
+            new JoinData
+            {
+                JoinNumber = 1,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Device Name",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Serial
+            });
 
         [JoinName("PresetNames")]
         public JoinDataComplete PresetNames = new JoinDataComplete(
@@ -132,15 +132,15 @@ namespace EssentialsPluginTemplate
                 }
             );
 
-		#endregion
+        #endregion
 
-		/// <summary>
-		/// Plugin device BridgeJoinMap constructor
-		/// </summary>
-		/// <param name="joinStart">This will be the join it starts on the EISC bridge</param>
+        /// <summary>
+        /// Plugin device BridgeJoinMap constructor
+        /// </summary>
+        /// <param name="joinStart">This will be the join it starts on the EISC bridge</param>
         public ExtronQuantumJoinMap(uint joinStart)
             : base(joinStart, typeof(ExtronQuantumJoinMap))
-		{
-		}
-	}
+        {
+        }
+    }
 }
